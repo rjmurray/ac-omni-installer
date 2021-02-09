8bf0d8e937f69cb77976c9f36c5ce1e5891dc11d
Ryan Murray <ryjmurray@gmail.com>
2021-02-08 22:32:53 -0500

    updated .gitignore and added main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+
+from pathlib import Path
+import os
+import patoolib
+
+def main():
+# %%
+    current_dir = Path(os.getcwd())
+    archive_dir = Path('c:/Users/rjm/Downloads/acoi_test')
+
+    for f in os.listdir(archive_dir):
+        pass
+    
